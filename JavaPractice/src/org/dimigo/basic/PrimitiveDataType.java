af7eeb48d00ab48403a4f43a304a3d281deabf3d
JuchanP <dhr05074@gmail.com>
2015-03-17 14:57:16 +0900

    Java 과제 : 3월 17일

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,33 @@
+/**
+ * << 첫번째 수행평가 과제 >>
+ * 내용 : 아이유 프로필 출력하기
+ * 작성일자 : 2015년 3월 13일
+ * @author JuchanP
+ * @version 1.0
+ */
+
+package org.dimigo.basic;
+
+public class PrimitiveDataType {
+	public static void main(String[] args) {
+		String name = "아이유";
+		boolean isman = false;
+		int age = 23;
+		double height = 161.8;
+		float weight = 44.3f;
+		char bltype = 'A';
+		
+		System.out.println("<< 아이유 프로필 >>");
+		
+		System.out.println("이름 : "+name);
+		
+		if (isman) System.out.println("성별 : 남자");
+		else System.out.println("성별 : 여자");
+
+		System.out.println("나이 : "+age+" 세");
+		System.out.println("키 : "+height+" cm");
+		System.out.println("몸무게 : "+weight+" kg");
+		System.out.println("혈액형 : "+bltype+" 형");
+		
+	}
+}
