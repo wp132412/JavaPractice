653e3f9190ebd4271b2e8a1bd3e630c3f4db880b
JuchanP <dhr05074@gmail.com>
2015-04-13 09:18:13 +0900

    OOP 과제

---
+++ JavaPractice/src/org/dimigo/oop/Car.java
@@ -0,0 +1,50 @@
+package org.dimigo.oop;
+
+public class Car {
+    private String company;
+    private String model;
+    private String color;
+    private int maxSpeed;
+    private int price;
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setCompany(String company2) {
+        company = company2;
+    }
+
+    public void setModel(String model2) {
+        model = model2;
+    }
+
+    public void setColor(String color2) {
+        color = color2;
+    }
+
+    public void setMaxSpeed(int maxSpeed2) {
+        maxSpeed = maxSpeed2;
+    }
+
+    public void setPrice(int price2) {
+        price = price2;
+    }
+}
+
