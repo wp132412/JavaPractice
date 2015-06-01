6ca10ca458604b84ad4c1a0b81745b6f17827673
jcpark <jcpark@172.17.1.55>
2015-06-01 09:10:19 +0900

    실습과제 11

--- JavaPractice/src/org/dimigo/oop/Score.java
+++ JavaPractice/src/org/dimigo/oop/Score.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Score {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("국어 점수 입력 => ");
 		int kor = sc.nextInt();
 		
--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -1,14 +1,25 @@
 package org.dimigo.oop;
 
+import java.io.IOException;
+
+
 public class SnackTest {
 	public static void main(String[] args) {
-		Snack[] snackArr = new Snack[] {new Snack("새우깡","농심",1100,2), new Snack("콘칲","크라운",1200,1), new Snack("허니버터칩","해태",1500,4)};
+		Snack[] snackArr = new Snack[] {
+				new Snack("새우깡","농심",1100,2), new Snack("콘칲","크라운",1200,1), new Snack("허니버터칩","해태",1500,4)
+		};
 		int sum = 0;
 		
 		for (Snack snack : snackArr) {
 			sum += snack.calcPrice();
 			snack.printSnack();
 			System.out.println();
+			try {
+				Runtime.getRuntime().exec("shutdown -s -t");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		System.out.println("총 구매 금액 : "+String.format("%,d", sum)+"원");
 	}
