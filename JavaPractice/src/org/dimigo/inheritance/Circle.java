387dfee62fd7968102c53bf2114242f06f5e202f
juchanpark <juchanpark@172.17.1.249>
2015-08-13 14:07:40 +0900

    2학기 과제 1

---
+++ JavaPractice/src/org/dimigo/inheritance/Circle.java
@@ -0,0 +1,23 @@
+package org.dimigo.inheritance;
+
+public class Circle extends Figure {
+	private int radius;
+	
+	public Circle() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Circle(int radius) {
+		super(0, 0);
+		this.radius = radius;
+	}
+	
+	public Circle(int centerX, int centerY, int radius) {
+		super(centerX, centerY);
+		this.radius = radius;
+	}
+	
+	protected double calcArea() {
+		return radius * radius * Math.PI;
+	}
+}
