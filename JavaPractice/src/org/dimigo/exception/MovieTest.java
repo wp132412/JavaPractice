73e0d78ea880e8f50b3a5c8d905857d4d4470eb2
Juchan Park <juchanpark@172.17.3.126>
2015-09-22 14:39:50 +0900

    Commit

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -2,7 +2,7 @@ package org.dimigo.exception;
 
 public class MovieTest {
 	public static void main(String[] args) {
-		Movie[] movies = { new Movie("사도", 12), new Movie("베테랑", 15), new Movie("앤트맨", 12) };
+		Movie[] movies = { new Movie("베테랑", 15), new Movie("앤트맨", 12), new Movie("사도", 12),  };
 
 		int age = 13;
 		for (Movie movie : movies) {
